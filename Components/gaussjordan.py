2d88e53e5228d29e032f270a85df6c512566911b
Fernando Partida <ferpart@hotmail.com>
2020-05-23 17:15:42 -0500

    gui finishied

---
+++ Components/gaussjordan.py
@@ -0,0 +1,3 @@
+def gauss_jordan(matrix, sol_matrix):
+    #inserta tu codigo para solucionar el gauss jordan aqui
+    return [6.1538, 1.5385]
---
+++ main.py
@@ -0,0 +1,7 @@
+from Components import App
+
+def main():
+    App()
+
+if __name__ == "__main__":
+    main()
